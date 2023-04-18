bbba4ec2826a5737391f53b3a03f1c553bf152b1
Mendel Zirkind <mendelzirkindfresno@gmail.com>
2023-04-17 22:01:40 -0400

    adds functions to open/close modal

--- src/Content.jsx
+++ src/Content.jsx
@@ -2,20 +2,32 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import { PostsNew } from "./PostsNew";
 import { PostsIndex } from "./PostsIndex";
+import { Modal } from "./Modal";
 
 export function Content() {
   const [posts, setPosts] = useState([]);
+  const [isPostsShowVisible, setIsPostsShowVisible] = useState(false);
   const handleIndexPosts = () => {
     axios.get("http://localhost:3000/posts.json").then(function (response) {
       console.log(response);
       setPosts(response.data);
     });
   };
+  const handleShowPost = () => {
+    setIsPostsShowVisible(true);
+  };
+
+  const handleClose = () => {
+    setIsPostsShowVisible(false);
+  };
   useEffect(handleIndexPosts, []);
   return (
     <div>
       <PostsNew />
-      <PostsIndex posts={posts} />
+      <PostsIndex posts={posts} onShowPost={handleShowPost} />
+      <Modal show={isPostsShowVisible} onClose={handleClose}>
+        <p>TEST</p>
+      </Modal>
     </div>
   );
 }
